e9e66a1c5dc503733797dbbe26065440bb7e189a
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-06-09 00:32:11 +0530

    chore: added lock file

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,6 +1,5 @@
 describe('[Index]', () => {
-    it('should validate 1+2=3', () => {
-        expect(1 + 2)
-            .toBe(3);
-    });
+  it('should validate 1+2=3', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
